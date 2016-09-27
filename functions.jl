79f2c60afa734e63b786391f326da2c81ab8cf67
kroy@sfu.ca <kroy@sfu.ca>
2016-09-27 05:36:08 +0000

    Exports functions after they are declared

--- functions.jl
+++ functions.jl
@@ -17,7 +17,7 @@ end
 
 function or(sX, sY)
 end
-function xor()
+function xor(sX, sY)
 end
 function add(sX, sY)
 println("adding s1,s2") #Debug print
@@ -60,9 +60,67 @@ function rr(sX)
 end
 function regbank(A::String)
 end
-function store()
+function store(k)
 end
-function fetch()
+function fetch(k)
 end
+#= EXPORT ------- =#
+export load
 
+
+export star
+
+
+export and
+
+
+
+export or
+
+export xor
+
+export add
+
+
+export addcy
+
+export sub
+
+
+export subcy
+
+export test
+
+export testcy
+
+export compare
+
+export comparecy
+
+export sl0
+
+export sl1
+
+export slx
+
+export sla
+
+export rl
+
+
+export sr0
+
+export sr1
+
+export srx
+
+export sra
+
+export rr
+
+export regbank
+
+export store
+
+export fetch
 end
