d36a190fb09f14ef97b486bfe145aa829e3657c5
kroy@sfu.ca <kroy@sfu.ca>
2016-09-28 06:31:01 +0000

    clarifies comment

--- Regbank.jl
+++ Regbank.jl
@@ -1,4 +1,7 @@
-module regbankA #Registers in bank A
+#=---------
+REGBANK A
+----------=#
+module regbankA 
 #=----------------------------
 Initialize registers to zero
 -----------------------------=#
@@ -25,7 +28,7 @@ Text parsing stuff (you can ignore :])
 registers = Dict("S0" => s0,"S1" => s1,"S2" => s2,"S3" => s3,"S4" => s4,"S5" => s5,"S6" => s6,
 "S7" => s7,"S8" => s8,"S9" => s9,"SA" => sA,"SB" => sB,"SC" => sC,"SD" => sD,"SE" => sE,"SF" => sF)
 #=----------------------------------=#
-
+#=----Getters and setters allow access to registers from other modules----=#
 function get(sX)
 return registers[sX]
 end
@@ -36,7 +39,9 @@ end
 
 end
 
-
+#=--------------
+REGBANK B
+---------------=#
 module regbankB #Registers in bank A
 #=----------------------------
 Initialize registers to zero
