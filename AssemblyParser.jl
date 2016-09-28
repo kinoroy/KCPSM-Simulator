0c60d99d559c1c9a65a3c64b3784646fcbd12db2
kroy@sfu.ca <kroy@sfu.ca>
2016-09-28 02:17:52 +0000

    Changed to allow instructions input file to start with 0-n comments

--- AssemblyParser.jl
+++ AssemblyParser.jl
@@ -25,6 +25,9 @@ lines = readlines(inStream) # Create an array with each element, an instruction
 counter = 1 #Start the counter
 
 for l in lines #Stepping through the lines
+    if(l[1] == ";") #Allows starting the file with comment only lines
+      continue
+    end
     l = split(l,r";")[1] #elimnate the comments (garbage)
     #Determines type of arguments in the line
 
