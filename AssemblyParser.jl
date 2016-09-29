50bb1ac193b5f188a5aa12adc80c647186af5892
rulai <rulai>
2016-09-29 02:17:35 +0000

    rename variable

--- AssemblyParser.jl
+++ AssemblyParser.jl
@@ -3,87 +3,87 @@ Created by: Kino Roy for CMPT 276, Assignment 1. =#
 module AssemblyParser
 
 function Parse()
-#=-----------------------------------------
-Initializing data structures for holding data
-------------------------------------------=#
-instructions = Array{AbstractString}(2048,4)
-labelDict = Dict() #Creates the dictionary for labels
+  #=-----------------------------------------
+  Initializing data structures for holding data
+  ------------------------------------------=#
+  instructions = Array{AbstractString}(2048,4)
+  labelDict = Dict() #Creates the dictionary for labels
 
-fill!(instructions, "")
-#=-----------------------------
-BEGIN FILE READING
-------------------------------=#
+  fill!(instructions, "")
 
-inStream = open("instructions.txt") #Open the input file and set the input stream
-lines = readlines(inStream) # Create an array with each element, an instruction
-close(inStream)
+  lines = readFile()
 
-counter = 1 #Start the counter
+  counter = 1 #Start the counter
 
-for l in lines
-  if(l[1] == ";") #Allows starting the file with comment only lines
-    continue
-  end
-  l = split(l,r";")[1] #eliminate the comments (garbage)
-  #Determines type of arguments in the line
+  for l in lines
+    if(l[1] == ";") #Allows starting the file with comment only lines
+      continue
+    end
+    l = split(l,r";")[1] #eliminate the comments (garbage)
+    #Determines type of arguments in the line
 
-  splitLine = split(l,r":| |,",keep=false) #Filters text
+    splitLine = split(l,r":| |,",keep=false) #Filters text
 
-#---- Begin Parsing ----#
+  #---- Begin Parsing ----#
 
-  #CASE: "INST"
-  if size(splitLine)[1] == 1
+    #CASE: "INST"
+    if size(splitLine)[1] == 1
 
-    instructions[counter,2] = chomp(splitLine[1])
+      instructions[counter,2] = chomp(splitLine[1])
 
-  #CASE: "LABEL + INST"
-  elseif size(splitLine)[1] == 2 && contains(l,":")
-    instructions[counter,1] = splitLine[1]
-    instructions[counter,2] = chomp(splitLine[2])
-    #ADDS LABEL TO DICTIONARY
-    labelDict["$(splitLine[1])"] = counter
+    #CASE: "LABEL + INST"
+    elseif size(splitLine)[1] == 2 && contains(l,":")
+      instructions[counter,1] = splitLine[1]
+      instructions[counter,2] = chomp(splitLine[2])
+      #ADDS LABEL TO DICTIONARY
+      labelDict["$(splitLine[1])"] = counter
 
 
-  #CASE: "INST + OP1"
-  elseif size(splitLine)[1] == 2
-    instructions[counter,2] = splitLine[1]
-    instructions[counter,3] = chomp(splitLine[2])
-    #ADDS LABEL TO DICTIONARY
-    labelDict["$(splitLine[1])"] = counter
+    #CASE: "INST + OP1"
+    elseif size(splitLine)[1] == 2
+      instructions[counter,2] = splitLine[1]
+      instructions[counter,3] = chomp(splitLine[2])
+      #ADDS LABEL TO DICTIONARY
+      labelDict["$(splitLine[1])"] = counter
 
-  #CASE: "LABEL + INST + OP1"
-  elseif size(splitLine)[1] == 3 && contains(l,":")
-    instructions[counter,1] = splitLine[1]
-    instructions[counter,2] = splitLine[2]
-    instructions[counter,3] = chomp(splitLine[3])
-    #ADDS LABEL TO DICTIONARY
-    labelDict["$(splitLine[1])"] = counter
+    #CASE: "LABEL + INST + OP1"
+    elseif size(splitLine)[1] == 3 && contains(l,":")
+      instructions[counter,1] = splitLine[1]
+      instructions[counter,2] = splitLine[2]
+      instructions[counter,3] = chomp(splitLine[3])
+      #ADDS LABEL TO DICTIONARY
+      labelDict["$(splitLine[1])"] = counter
 
-  #CASE: "INST + OP1 + OP2"
-  elseif size(splitLine)[1] == 3 && contains(l,",")
-    instructions[counter,2] = splitLine[1]
-    instructions[counter,3] = splitLine[2]
-    instructions[counter,4] = chomp(splitLine[3])
+    #CASE: "INST + OP1 + OP2"
+    elseif size(splitLine)[1] == 3 && contains(l,",")
+      instructions[counter,2] = splitLine[1]
+      instructions[counter,3] = splitLine[2]
+      instructions[counter,4] = chomp(splitLine[3])
 
-  #CASE: "LABEL + INST + OP1 + OP2"
-  elseif size(splitLine)[1] == 4
-    instructions[counter,1] = splitLine[1]
-    instructions[counter,2] = splitLine[2]
-    instructions[counter,3] = splitLine[3]
-    instructions[counter,4] = chomp(splitLine[4])
-    #ADDS LABEL TO DICTIONARY
-    labelDict["$(splitLine[1])"] = counter
-  end
+    #CASE: "LABEL + INST + OP1 + OP2"
+    elseif size(splitLine)[1] == 4
+      instructions[counter,1] = splitLine[1]
+      instructions[counter,2] = splitLine[2]
+      instructions[counter,3] = splitLine[3]
+      instructions[counter,4] = chomp(splitLine[4])
+      #ADDS LABEL TO DICTIONARY
+      labelDict["$(splitLine[1])"] = counter
+    end
 
-   counter += 1
+     counter += 1
+  end #endfor
+
+  return instructions,labelDict
 end
 
-#=-----------
- END READING
- -----------=#
+function readFile()
+  stream = open("instructions.txt") #Open the input file and set the input stream
+  lines = readlines(stream) # Create an array with each element, an instruction
+  close(stream)
 
-return instructions,labelDict
+  return lines
 end
+
 end
 #=------------
 END ASSEMBLY PARSER
