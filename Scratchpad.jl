8e144b942f3e6f62d7345cc431dfb1f2ead360f7
kroy@sfu.ca <kroy@sfu.ca>
2016-10-04 00:04:42 +0000

    Template for scratchpad

---
+++ Scratchpad.jl
@@ -0,0 +1,9 @@
+module scratchpad
+
+function store(sX,ss) #Should store value sX, into ss. #Both sX, and ss are passed as integer value
+end
+
+function fetch(ss) #Should return the value at address ss #ss is passed by value
+end
+
+end
