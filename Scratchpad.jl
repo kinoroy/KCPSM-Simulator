58c82a79f245722c250c8503be8a40ad4d379f55
rulai <rulai>
2016-10-04 04:54:28 +0000

    Remove println

--- Scratchpad.jl
+++ Scratchpad.jl
@@ -3,7 +3,6 @@ module scratchpad
 sp = zeroes(UInt8, 256)
 
 function store(sX,ss) #Should store value sX, into ss. #Both sX, and ss are passed as integer value
-    println(0xff & ss)
     sp[(0xff & ss) + 1] = sX
 end
 
